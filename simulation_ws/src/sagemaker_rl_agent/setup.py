026e788f9b52d1b99c264352b5ded8e09eafa8d4
Chris Rhodes <crr0004@users.noreply.github.com>
2019-04-04 14:23:01 +1100

    Updates setup.py to explicitly set zip_safe

--- simulation_ws/src/sagemaker_rl_agent/setup.py
+++ simulation_ws/src/sagemaker_rl_agent/setup.py
@@ -9,6 +9,7 @@ REQUIRES_PYTHON = '>=3.5.0'
 setup(
     name=NAME,
     version='0.0.1',
+    zip_safe=False,
     packages=find_packages(),
     python_requires=REQUIRES_PYTHON,
     install_requires=[
